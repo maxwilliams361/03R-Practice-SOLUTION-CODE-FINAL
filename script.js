9674fa86e9264240bbf121dcbbb0686a02c1db24
maxwilliams361 <52715538+maxwilliams361@users.noreply.github.com>
2022-07-16 11:39:19 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+let buttonOne= document.querySelector(".option-one");
+let openBoxDiv=document.querySelector(".option-one-screen");
+let buttonTwo=document.querySelector(".option-two");
+let runAwayDiv=document.querySelector(".option-two-screen");
+
+
+
+buttonOne.onclick=function(){
+   openBoxDiv.style.display="block";
+};
+
+buttonTwo.onclick=function(){
+    runAwayDiv.style.display="block";
+};
+
+
